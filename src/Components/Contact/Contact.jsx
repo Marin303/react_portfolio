b11f2937ec7a215cbe6cdeee473914048a0b1a13
Marin <marin.muktic3@gmail.com>
2023-05-10 21:17:01 +0200

    weather component initial

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,15 +10,15 @@ const Contact = ({ isActive }) => {
       <h2>Contact me</h2>
       <div className="ContactContainer">
         <a href="https://www.facebook.com/muktic2">
-          <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>Facebok
+          <i className="fa fa-facebook-official fa-2x" aria-hidden="true"></i>Facebok
         </a>
         <br />
         <a href="https://www.linkedin.com/in/marin-muktic/">
-          <i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>Linkedin
+          <i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>Linkedin
         </a>
         <br />
         <a href="https://github.com/Marin303">
-          <i class="fa fa-github fa-2x" aria-hidden="true"></i>Github
+          <i className="fa fa-github fa-2x" aria-hidden="true"></i>Github
         </a>
       </div>
     </div>
--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-
-const Weather = ({isActive}) => {
-    if(!isActive){
-        return null
-    }
+import React from "react";
+import ChatCss from '../../Components/Chat/Chat.module.css'
+const Weather = ({ isActive }) => {
+  if (!isActive) {
+    return null;
+  }
   return (
-    <div>Weather</div>
-  )
-}
+    <div>
+      <div className="WeatherContainer">
+        <h1>Weather</h1>
+        <input type="text" />
+        <button className={ChatCss.ChatBtn}>SEARCH</button>
+      </div>
+    </div>
+  );
+};
 
-export default Weather
+export default Weather;
